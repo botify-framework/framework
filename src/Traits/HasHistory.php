f883316fade723aa72a3cf991badab23e64be541
lorddeveloper <losrkzpp@hi2.in>
2022-09-06 18:06:51 +0430

    Renamed readHistory to getHistory, Fixed return type problem

--- src/Traits/HasHistory.php
+++ src/Traits/HasHistory.php
@@ -3,11 +3,12 @@
 namespace Botify\Traits;
 
 use Amp\Producer;
+use Amp\Promise;
 
 trait HasHistory
 {
 
-    public function readHistory(callable $filter = null, int $limit = 100): Producer
+    public function getHistory(callable $filter = null, int $limit = 100): Promise
     {
         return $this->getAPI()->getHistory(
             $this->id, $filter, $limit
